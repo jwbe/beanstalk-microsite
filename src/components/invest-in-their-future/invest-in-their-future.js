f1c2955a835c552cd1c6791189cc13319d9f6759
JamesBarrett <james.barrett.works@gmail.com>
2019-11-18 15:40:55 +0300

    Update

--- src/components/invest-in-their-future/invest-in-their-future.js
+++ src/components/invest-in-their-future/invest-in-their-future.js
@@ -1,9 +1,161 @@
 import React from 'react';
 import styles from './invest-in-their-future.module.scss';
+import titleStyles from '../info-title-styles/info-title-styles.module.scss';
 
 const InvestInTheirFuture = () => {
   return (
-    
+    <>
+      <div className={titleStyles.TitleBlock}>
+        <h1 className={titleStyles.TitleBlock_heading}>
+          Invest in their future
+        </h1>
+        <h2 className={titleStyles.TitleBlock_subheading}>
+          The new, really easy way to build up a<br/> fund to give your child a launch into adult life
+        </h2>
+      </div>
+      <div className={styles.Stem_container}>
+        <div className={`${styles.StemWrapper} ${styles.StemWrapper___vertical} ${styles.StemWrapper___desktop} ${styles.StemWrapper___width50}`}>
+          <div className={`${styles.Stem_top_leaves}`}>
+          </div>
+          <div className={`${styles.Stem___vertical}`}>
+          </div>
+        </div>
+        <div className={`${styles.StemWrapper} ${styles.StemWrapper___vertical} ${styles.StemWrapper___width50}`}>
+        </div>
+        <div className={`${styles.StemWrapper___width50}`}>
+          <div className={`${styles.Stem___horizontal} ${styles.Stem___connectVertical}`}>
+            <div className={`${styles.Stem_section}`}>
+              <div className={`${styles.Stem_section_row}`}>
+                <div className={`${styles.Stem_curve_shim}`}>
+                </div>
+              </div>
+              <div className={`${styles.Stem_section_row}`}>
+                <div className={`${styles.Stem_curve} ${styles.Stem_curve_left}`}>
+                </div>
+              </div>
+            </div>
+            <div className={`${styles.Stem_section} ${styles.Stem_section___grow}`}>
+              <div className={`${styles.Stem_section_row___middle}`}>
+                <div className={`${styles.Stem_curve} ${styles.Stem_curve_middle}`}>
+                  <div className={`${styles.Stem_doubleLeaves} ${styles.Stem_doubleLeaves___mobileEnd___top}`}>
+                  </div>
+                </div>
+              </div>
+              <div className={`${styles.Stem_section_row___middle}`}>
+              </div>
+            </div>
+            <div className={`${styles.Stem_section} ${styles.Stem_section___desktop}`}>
+              <div className={`${styles.Stem_section_row}`}>
+                <div className={`${styles.Stem_curve} ${styles.Stem_curve_right}`}>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={`${styles.Stem_textPod}`}>
+          We have no charges and one of the lowest fees on the market - only 0.5%.
+        </div>
+        <div className={`${styles.Stem___horizontal} ${styles.Foliage___flip180}`}>
+          <div className={`${styles.Stem_section}`}>
+            <div className={`${styles.Stem_section_row}`}>
+              <div className={`${styles.Stem_curve_shim}`}>
+              </div>
+            </div>
+            <div className={`${styles.Stem_section_row}`}>
+              <div className={`${styles.Stem_curve} ${styles.Stem_curve_left}`}>
+                <div className={`${styles.Stem_leaves} ${styles.Foliage___flip180}`}></div>
+              </div>
+            </div>
+          </div>
+          <div className={`${styles.Stem_section} ${styles.Stem_section___grow}`}>
+            <div className={`${styles.Stem_section_row___middle}`}>
+              <div className={`${styles.Stem_curve} ${styles.Stem_curve_middle}`}>
+                <div className={`${styles.Stem_doubleLeaves} ${styles.Foliage___flip180}`}>
+                </div>
+              </div>
+            </div>
+            <div className={`${styles.Stem_section_row___middle}`}>
+            </div>
+          </div>
+          <div className={`${styles.Stem_section}`}>
+            <div className={`${styles.Stem_section_row}`}>
+              <div className={`${styles.Stem_curve} ${styles.Stem_curve_right}`}></div>
+            </div>
+          </div>
+        </div>
+        <div className={`${styles.Stem_textPod} ${styles.Stem_textPod___right}`}>
+          Save little and often with round-ups or top up when the going is good.
+        </div>
+        <div className={`${styles.Stem___horizontal}`}>
+          <div className={`${styles.Stem_section}`}>
+            <div className={`${styles.Stem_section_row}`}>
+              <div className={`${styles.Stem_curve_shim}`}>
+              </div>
+            </div>
+            <div className={`${styles.Stem_section_row}`}>
+              <div className={`${styles.Stem_curve} ${styles.Stem_curve_left}`}>
+                <div className={`${styles.Stem_leaves} ${styles.Foliage___flip180}`}></div>
+              </div>
+            </div>
+          </div>
+          <div className={`${styles.Stem_section} ${styles.Stem_section___grow}`}>
+            <div className={`${styles.Stem_section_row___middle}`}>
+              <div className={`${styles.Stem_curve} ${styles.Stem_curve_middle}`}>
+                <div className={`${styles.Stem_doubleLeaves} ${styles.Foliage___flip180}`}>
+                </div>
+              </div>
+            </div>
+            <div className={`${styles.Stem_section_row___middle}`}>
+            </div>
+          </div>
+          <div className={`${styles.Stem_section}`}>
+            <div className={`${styles.Stem_section_row}`}>
+              <div className={`${styles.Stem_curve} ${styles.Stem_curve_right}`}></div>
+            </div>
+          </div>
+        </div>
+        <div className={`${styles.Stem_textPod}`}>
+          Grow your child's savings by investing with two simple choices.
+        </div>
+        <div className={`${styles.StemWrapper___width50}`}>
+          <div className={`${styles.Stem___horizontal} ${styles.Foliage___flip180}`}>
+            <div className={`${styles.Stem_section} ${styles.Stem_section___desktop}`}>
+              <div className={`${styles.Stem_section_row}`}>
+                <div className={`${styles.Stem_curve_shim}`}>
+                </div>
+              </div>
+              <div className={`${styles.Stem_section_row}`}>
+                <div className={`${styles.Stem_curve} ${styles.Stem_curve_left}`}></div>
+              </div>
+            </div>
+            <div className={`${styles.Stem_section} ${styles.Stem_section___grow}`}>
+              <div className={`${styles.Stem_section_row___middle}`}>
+                <div className={`${styles.Stem_curve} ${styles.Stem_curve_middle} ${styles.Stem_curve_middle___mobileEnd}`}>
+                  <div className={`${styles.Stem_doubleLeaves} ${styles.Stem_doubleLeaves___mobileEnd___bottom} ${styles.Foliage___flip180}`}>
+                  </div>
+                </div>
+              </div>
+              <div className={`${styles.Stem_section_row___middle}`}>
+              </div>
+            </div>
+            <div className={`${styles.Stem_section}`}>
+              <div className={`${styles.Stem_section_row}`}>
+                <div className={`${styles.Stem_curve} ${styles.Stem_curve_right}`}></div>
+              </div>
+              <div className={`${styles.Stem_section_row}`}>
+                <div className={`${styles.Stem_curve_shim}`}>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={titleStyles.TitleBlock}>
+        <h2 className={titleStyles.TitleBlock_subheading}>
+          Celebrate a tax-fee nest egg to help your <br/>child start adult life
+        </h2>
+      </div>
+    </>
   );
 };
 
---
+++ src/content/pages/invest-in-their-future.md
@@ -0,0 +1,8 @@
+---
+slug: invest-in-their-future
+heading: Invest In Their Future
+plain: true
+---
+import InvestInTheirFuture from '../../components/invest-in-their-future/invest-in-their-future';
+
+<InvestInTheirFuture/>
