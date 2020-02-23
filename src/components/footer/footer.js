e20f7a584e3b751767580bd397b469fad7b1f190
JamesBarrett <james.barrett.works@gmail.com>
2020-02-24 00:24:41 +0100

    update

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -12,12 +12,12 @@ import WaitingList from '../homepage/waiting-list/waiting-list';
 import iconFacebookSecondary from '../../images/icons/facebook-icon-secondary.png';
 import iconInstagramSecondary from '../../images/icons/instagram-icon-secondary.png';
 import iconTwitterSecondary from '../../images/icons/twitter-icon-secondary.png';
-//import iconLinkedInSecondary from '../../images/icons/linkedin-icon-secondary.png';
+import iconLinkedInSecondary from '../../images/icons/linkedin-icon-secondary.png';
 
 import iconFacebookWhite from '../../images/icons/facebook-icon-white.png';
 import iconInstagramWhite from '../../images/icons/instagram-icon-white.png';
 import iconTwitterWhite from '../../images/icons/twitter-icon-white.png';
-//import iconLinkedInWhite from '../../images/icons/linkedin-icon-white.png';
+import iconLinkedInWhite from '../../images/icons/linkedin-icon-white.png';
 
 /*
 const Footer = () => {
@@ -123,6 +123,10 @@ const Footer = () => {
               <a href="https://twitter.com/TheBeanstalkApp" target="_blank">
                 <img className={styles.Homepage_footer_socialIcons_desktop} src={iconTwitterSecondary} width="24" height="24"/>
                 <img className={styles.Homepage_footer_socialIcons_mobile} src={iconTwitterWhite} width="24" height="24"/>
+              </a>
+              <a href="https://www.linkedin.com/company/the-beanstalk-app" target="_blank">
+                <img className={styles.Homepage_footer_socialIcons_desktop} src={iconLinkedInSecondary} width="24" height="24"/>
+                <img className={styles.Homepage_footer_socialIcons_mobile} src={iconLinkedInWhite} width="24" height="24"/>
               </a>
             </div>
           </div>
