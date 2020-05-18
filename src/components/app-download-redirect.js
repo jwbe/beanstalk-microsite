f44622db55cd2b28977b7a74fe338e2cb3590893
JamesBarrett <james.barrett.works@gmail.com>
2020-05-18 18:01:05 +0100

    Update

--- src/components/app-download-redirect.js
+++ src/components/app-download-redirect.js
@@ -1,8 +1,42 @@
 import React, { Component } from 'react';
 import { isIOS, isAndroid, isBrowser } from 'react-device-detect';
 
-
 const AppDownloadRedirect = () => {
+  if (typeof window !== `undefined`) {
+    
+    var redirectFired = false;
+
+    function redirectToAppleStore() {
+      if (!redirectFired) {
+        redirectFired = true;
+        window.location.replace(`https://apps.apple.com/gb/app/beanstalk-save-invest/id1470619597`);
+      }
+    }
+
+    function redirectToGooglePlay() {
+      if (!redirectFired) {
+        redirectFired = true;
+        window.location.replace(`https://play.google.com/store/apps/details?id=com.beanstalk`);
+      }
+    }
+
+    if(isBrowser){
+      window.location.replace(`https://beanstalkapp.co.uk`);
+    }
+
+    if(isIOS){
+      window.gtag('event', 'directed_to_appleStore', {
+        'event_callback': redirectToAppleStore
+      });
+    }
+
+    if(isAndroid){
+      window.gtag('event', 'directed_to_googlePlay', {
+        'event_callback': redirectToGooglePlay
+      });
+    }
+
+  }
   return <></>
 }
 
