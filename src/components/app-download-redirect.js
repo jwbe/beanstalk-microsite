b3e73cb443704de595176491110cc39dc0d81d95
JamesBarrett <james.barrett.works@gmail.com>
2020-05-18 17:51:49 +0100

    Update

---
+++ src/components/app-download-redirect.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { isIOS, isAndroid, isBrowser } from 'react-device-detect';
+
+
+const AppDownloadRedirect = () => {
+  return <></>
+}
+
+export default AppDownloadRedirect;
