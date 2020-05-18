bafd93605942bb8600bc8cc2abb6ea26f8eced3c
JamesBarrett <james.barrett.works@gmail.com>
2020-05-18 16:41:55 +0100

    Update

--- src/components/app-download-redirect.js
+++ src/components/app-download-redirect.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import { isIOS, isAndroid, isBrowser } from 'react-device-detect';
 
-class AppDownloadRedirect extends React.Component {
-  componentDidMount() {
-    window.location.replace("https://apps.apple.com/gb/app/beanstalk-save-invest/id1470619597");
+
+if (typeof window !== `undefined`) {
+  if(isBrowser){
+    window.location.replace(`https://beanstalkapp.co.uk`);
+  }
+
+  if(isIOS){
+    window.location.replace(`https://apps.apple.com/gb/app/beanstalk-save-invest/id1470619597`);
+  }
+
+  else if(isAndroid){
+    window.location.replace(`https://play.google.com/store/apps/details?id=com.beanstalk`);
   }
 }
 
+const AppDownloadRedirect = () => {
+  return <></>
+}
 
 export default AppDownloadRedirect;
