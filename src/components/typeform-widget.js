b039d85820150485ffe6a5b376bfcddee5df2658
JamesBarrett <james.barrett.works@gmail.com>
2020-06-08 11:43:28 +0100

    Update

---
+++ src/components/typeform-widget.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { ReactTypeformEmbed } from 'react-typeform-embed';
+
+const TypeformWidget = () => {
+  const style = {
+    height: '500px'
+  }
+  return (
+    <div style={style}>
+      <ReactTypeformEmbed url="https://kidstartuk.typeform.com/to/kND1YS"/>;
+    </div>
+  );
+} 
+
+export default TypeformWidget;
---
+++ src/content/pages/beanstalk-survey.md
@@ -0,0 +1,9 @@
+---
+slug: beanstalk-survey
+heading: 
+plain: true
+---
+
+import TypeformWidget from '../../components/typeform-widget'
+
+<TypeformWidget/>
