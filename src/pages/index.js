7add6c11ac59f620d288327842180dbbb877a50e
JamesBarrett <james.barrett.works@gmail.com>
2020-06-10 21:38:14 +0100

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,12 +19,9 @@ const IndexPage = () => {
           <meta name="smartbanner:title" content="Beanstalk App"/>
           <meta name="smartbanner:author" content="KidStart Ltd"/>
           <meta name="smartbanner:price" content="FREE"/>
-          <meta name="smartbanner:price-suffix-apple" content=" - On the App Store"/>
           <meta name="smartbanner:price-suffix-google" content=" - In Google Play"/>
-          <meta name="smartbanner:icon-apple" content="https://beanstalkapp.co.uk/images/beanstalk-icon.png"/>
           <meta name="smartbanner:icon-google" content="https://beanstalkapp.co.uk/images/beanstalk-icon.png"/>
           <meta name="smartbanner:button" content="VIEW"/>
-          <meta name="smartbanner:button-url-apple" content="https://apps.apple.com/gb/app/beanstalk-save-invest/id1470619597"/>
           <meta name="smartbanner:button-url-google" content="https://play.google.com/store/apps/details?id=com.beanstalk"/>
           <meta name="smartbanner:enabled-platforms" content="android"/>
           <meta name="smartbanner:close-label" content="Close"/>
