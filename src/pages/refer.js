b95a337ee010e29e3fdd2da37eff330417cc3dd8
JamesBarrett <james.barrett.works@gmail.com>
2020-08-10 01:02:23 +0200

    Update

--- src/pages/refer.js
+++ src/pages/refer.js
@@ -1,18 +1,21 @@
 import React from 'react'
+import Layout from '../templates/homepage'
 import { Router, Location } from '@reach/router'
 
 import Refer from '../components/refer/refer';
 
 const ReferPage = props => (
-  <>
-    <Location>
-      {({ location }) => (
-        <Router location={location}>
-          <Refer path="refer/:referID"/>
-        </Router>
-      )}
-    </Location>
-  </>
+  <Layout>
+    <>
+      <Location>
+        {({ location }) => (
+          <Router location={location}>
+            <Refer path="refer/:referID"/>
+          </Router>
+        )}
+      </Location>
+    </>
+  </Layout>
 )
 
 export default ReferPage;
