bfd413f7d37fb7b962639c5f693b5015fb269d86
JamesBarrett <james.barrett.works@gmail.com>
2020-08-10 19:23:41 +0200

    Update

--- src/components/refer/refer.js
+++ src/components/refer/refer.js
@@ -7,10 +7,12 @@ import { Header, HEADER_VARIANT } from '../header/header';
 import Footer from '../footer/footer';
 import ReferTheme from './refer.module.scss';
 
+import AppStoreLogo from './images/app-store-logo.png';
+import GooglePlayStoreLogo from './images/google-play-store-logo.png';
+
 class Refer extends Component {
   constructor(props) {
     super(props)
-    this.domRef = React.createRef()
     this.state = { formSubmitted: null }
   }
 
@@ -32,7 +34,6 @@ class Refer extends Component {
         this.setState({
           formSubmitted: 'true'
         })
-        this.domRef.current.reset()
       })
       .catch(err =>
         this.setState({
@@ -46,45 +47,64 @@ class Refer extends Component {
       <>
         <Head title="You've been invited to join Beanstalk!"/>
         <Header/>
-        <div className={ReferTheme.Container}>
-          <div className={ReferTheme.SignUp}>
-            <div className={ReferTheme.SignUp_Inner}>
-              {
-                this.state.formSubmitted ? <p>Form submitted</p>
-                :
-                <>
-                  <header className={ReferTheme.Header}>
-                    <h1 className={ReferTheme.SignUp_Heading}>
-                      You've been invited to join Beanstalk!
-                    </h1>
-                    <div className={ReferTheme.SignUp_Subheading}>
-                      <strong>2 minutes to open an account.</strong>
-                      <p>Please enter your email to claim your prize.</p>
-                    </div>
-                  </header>
+        <div className={ReferTheme.SignUp}>
+          <div className={ReferTheme.SignUp_Inner}>
+            {
+              this.state.formSubmitted 
+              ?
+              <>
+                <header className={ReferTheme.Header}>
+                  <h1 className={ReferTheme.SignUp_Heading}>
+                    Thank you.
+                  </h1>
+                  <div className={ReferTheme.SignUp_Subheading}>
+                    <strong>Now just download the Beanstalk app and <br className={ReferTheme.SignUp_Subheading_Break}/>register with the same email you just gave us.</strong>
+                    <p>It takes just a couple of minutes to open an account.</p>
+                  </div>
+                </header>
+                <div className={ReferTheme.Badges}>
+                  <a className={ReferTheme.Badges_Badge} href="https://apps.apple.com/gb/app/beanstalk-save-invest/id1470619597" target="_blank">
+                    <img className={ReferTheme.Badges_Badge_Image} src={AppStoreLogo} alt="Apple App Store logo" width="135" height="40"/>
+                  </a>
+                  <a className={ReferTheme.Badges_Badge} href="https://play.google.com/store/apps/details?id=com.beanstalk" target="_blank">
+                    <img className={ReferTheme.Badges_Badge_Image} src={GooglePlayStoreLogo} alt="Google Play Store logo" width="135" height="40"/>
+                  </a>
+                </div>
+                <p className={ReferTheme.SignUp_NotaBene}>
+                  <strong>Note:</strong> Your referral link will expire after 60 days.
+                </p>
+              </>
+              :
+              <>
+                <header className={ReferTheme.Header}>
+                  <h1 className={ReferTheme.SignUp_Heading}>
+                    You've been invited to join Beanstalk!
+                  </h1>
+                  <div className={ReferTheme.SignUp_Subheading}>
+                    <strong>2 minutes to open an account.</strong>
+                    <p>Please enter your email to claim your prize.</p>
+                  </div>
+                </header>
 
-                  <form 
-                  ref={this.domRef} 
-                  name="refer" 
-                  method="POST" 
-                  data-netlify="true" data-netlify-honeypot="bot-field" 
-                  onSubmit={event => this.handleSubmit(event)}
-                  >
-                    <div className={ReferTheme.Form_VisibleInputs}>
-                      <input className={ReferTheme.Form_Input} placeholder="Your email address" type="email" name="email" id="email" ref="email"/>
-                      <input className={ReferTheme.Form_Input} placeholder="Your full name" type="text" name="name" id="name" ref="name"/>
-                    </div>
-                    <input type="hidden" name='bot-field'/>
-                    <input type="hidden" ref="form-name" name="form-name" value="refer"/>
-                    <input type="hidden" ref="referID" name="referID" value={this.props.referID} id="referID"/>
-                    <button className={ReferTheme.Form_Submit} type="submit">Get Started!</button>
-                  </form>
-                  <p className={ReferTheme.SignUp_NB}>
-                    <strong>Note:</strong> We will only use your email to remind you to register and to record whether you have registered.
-                  </p>
-                </>
-              }
-            </div>
+                <form 
+                name="refer" 
+                method="POST" 
+                data-netlify="true" data-netlify-honeypot="bot-field" 
+                onSubmit={event => this.handleSubmit(event)}>
+                  <div className={ReferTheme.Form_VisibleInputs}>
+                    <input className={ReferTheme.Form_Input} placeholder="Your email address" type="email" name="email" id="email" ref="email"/>
+                    <input className={ReferTheme.Form_Input} placeholder="Your full name" type="text" name="name" id="name" ref="name"/>
+                  </div>
+                  <input type="hidden" ref="bot-field" name="bot-field"/>
+                  <input type="hidden" ref="form-name" name="form-name" value="refer"/>
+                  <input type="hidden" ref="referID" name="referID" value={this.props.referID} id="referID"/>
+                  <button className={ReferTheme.Form_Submit} type="submit">Get Started!</button>
+                </form>
+                <p className={ReferTheme.SignUp_NotaBene}>
+                  <strong>Note:</strong> We will only use your email to remind you to register and to record whether you have registered.
+                </p>
+              </>
+            }
           </div>
         </div>
 
