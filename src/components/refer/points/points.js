4c007295ae09856a752749fdf233314453df13b1
JamesBarrett <james.barrett.works@gmail.com>
2020-08-11 10:54:33 +0200

    Update

---
+++ src/components/refer/points/points.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import PointsTheme from './points.module.scss'
+
+import PadlockIcon from './images/padlock-icon.svg'
+import PiggybankIcon from './images/piggybank-icon.svg'
+import FamilyIcon from './images/family-icon.svg'
+import CoinInHandIcon from './images/coin-in-hand-icon.svg'
+
+const Points = () => (
+  <div className={PointsTheme.Points}>
+    <div className={PointsTheme.Point}>
+      <div className={PointsTheme.Point_Icon_Wrapper}>
+        <img className={PointsTheme.Point_Icon} src={FamilyIcon}/>
+      </div>
+      <div className={PointsTheme.Point_Content}>
+        <h3 className={PointsTheme.Point_Heading}>
+          All accounts in one place
+        </h3>
+        <p className={PointsTheme.Point_Description}>
+          See everyone you are saving for, how much they have and your contributions all within seconds
+        </p>
+      </div>
+    </div>
+    <div className={PointsTheme.Point}>
+      <div className={PointsTheme.Point_Icon_Wrapper}>
+        <img className={PointsTheme.Point_Icon} src={PiggybankIcon}/>
+      </div>
+      <div className={PointsTheme.Point_Content}>
+        <h3 className={PointsTheme.Point_Heading}>
+          Start small 
+        </h3>
+        <p className={PointsTheme.Point_Description}>
+          No contributions needed to open an account
+        </p>
+      </div>
+    </div>
+    <div className={PointsTheme.Point}>
+      <div className={PointsTheme.Point_Icon_Wrapper}>
+        <img className={PointsTheme.Point_Icon} src={CoinInHandIcon}/>
+      </div>
+      <div className={PointsTheme.Point_Content}>
+        <h3 className={PointsTheme.Point_Heading}>
+          Many ways to save
+        </h3>
+        <p className={PointsTheme.Point_Description}>
+          Round ups, free money back on your shopping, easy top ups
+        </p>
+      </div>
+    </div>
+    <div className={PointsTheme.Point}>
+      <div className={PointsTheme.Point_Icon_Wrapper}>
+        <img className={PointsTheme.Point_Icon} src={FamilyIcon}/>
+      </div>
+      <div className={PointsTheme.Point_Content}>
+        <h3 className={PointsTheme.Point_Heading}>
+          Link family and friends
+        </h3>
+        <p className={PointsTheme.Point_Description}>
+          Boost your kids savings with multiple people contributing
+        </p>
+      </div>
+    </div>
+    <div className={PointsTheme.Point}>
+      <div className={PointsTheme.Point_Icon_Wrapper}>
+        <img className={PointsTheme.Point_Icon} src={PadlockIcon}/>
+      </div>
+      <div className={PointsTheme.Point_Content}>
+        <h3 className={PointsTheme.Point_Heading}>
+          Safe and secure
+        </h3>
+        <p className={PointsTheme.Point_Description}>
+          Beanstalk is FCA regulated and your money and investments are held under FCA client money and custody rules by our authorised investment platform provider
+        </p>
+      </div>
+    </div>
+  </div>
+)
+
+export default Points;
--- src/components/refer/refer.js
+++ src/components/refer/refer.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import * as qs from 'query-string';
-import { Router, Location } from '@reach/router';
-import Head from '../head/head';
-import { Header, HEADER_VARIANT } from '../header/header';
-import Footer from '../footer/footer';
-import ReferTheme from './refer.module.scss';
+import React, { Component } from 'react'
+import axios from 'axios'
+import * as qs from 'query-string'
+import { Router, Location } from '@reach/router'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
-import AppStoreLogo from './images/app-store-logo.png';
-import GooglePlayStoreLogo from './images/google-play-store-logo.png';
+import Head from '../head/head'
+import { Header } from '../header/header'
+import Footer from '../footer/footer'
+import ReferTheme from './refer.module.scss'
+
+import Points from './points/points'
+
+import AppStoreLogo from './images/app-store-logo.png'
+import GooglePlayStoreLogo from './images/google-play-store-logo.png'
 
 class Refer extends Component {
   constructor(props) {
@@ -108,78 +112,11 @@ class Refer extends Component {
           </div>
         </div>
 
-        <div className={ReferTheme.Content}>
-          <h2 className={ReferTheme.Content_Heading}>
-            The easiest way to build a nest egg for yourself, <br className={ReferTheme.Content_Heading_Break}/>your children or your grandchildren
-          </h2>
-          <div className={ReferTheme.Points}>
-            <div className={ReferTheme.Point}>
-              <div className={ReferTheme.Point_Icon_Wrapper}>
-                <div className={ReferTheme.Point_Icon}></div>
-              </div>
-              <div className={ReferTheme.Point_Content}>
-                <h3 className={ReferTheme.Point_Heading}>
-                  All accounts in one place
-                </h3>
-                <p className={ReferTheme.Point_Description}>
-                  See everyone you are saving for, how much they have and your contributions all within seconds
-                </p>
-              </div>
-            </div>
-            <div className={ReferTheme.Point}>
-              <div className={ReferTheme.Point_Icon_Wrapper}>
-                <div className={ReferTheme.Point_Icon}></div>
-              </div>
-              <div className={ReferTheme.Point_Content}>
-                <h3 className={ReferTheme.Point_Heading}>
-                  Start small 
-                </h3>
-                <p className={ReferTheme.Point_Description}>
-                  No contributions needed to open an account
-                </p>
-              </div>
-            </div>
-            <div className={ReferTheme.Point}>
-              <div className={ReferTheme.Point_Icon_Wrapper}>
-                <div className={ReferTheme.Point_Icon}></div>
-              </div>
-              <div className={ReferTheme.Point_Content}>
-                <h3 className={ReferTheme.Point_Heading}>
-                  Many ways to save
-                </h3>
-                <p className={ReferTheme.Point_Description}>
-                  Round ups, free money back on your shopping, easy top ups
-                </p>
-              </div>
-            </div>
-            <div className={ReferTheme.Point}>
-              <div className={ReferTheme.Point_Icon_Wrapper}>
-                <div className={ReferTheme.Point_Icon}></div>
-              </div>
-              <div className={ReferTheme.Point_Content}>
-                <h3 className={ReferTheme.Point_Heading}>
-                  Link family and friends
-                </h3>
-                <p className={ReferTheme.Point_Description}>
-                  Boost your kids savings with multiple people contributing
-                </p>
-              </div>
-            </div>
-            <div className={ReferTheme.Point}>
-              <div className={ReferTheme.Point_Icon_Wrapper}>
-                <div className={ReferTheme.Point_Icon}></div>
-              </div>
-              <div className={ReferTheme.Point_Content}>
-                <h3 className={ReferTheme.Point_Heading}>
-                  Safe and secure
-                </h3>
-                <p className={ReferTheme.Point_Description}>
-                  Beanstalk is FCA regulated and your money and investments are held under FCA client money and custody rules by our authorised investment platform provider
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+        <h2 className={ReferTheme.Content_Heading}>
+          The easiest way to build a nest egg for yourself, <br className={ReferTheme.Content_Heading_Break}/>your children or your grandchildren
+        </h2>
+
+        <Points/>
 
         <Footer/>
       </>
