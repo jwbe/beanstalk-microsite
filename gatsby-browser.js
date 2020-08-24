4bf88b367774c2ca2f5e94abedaadb0b2b637a32
JamesBarrett <james.barrett.works@gmail.com>
2020-08-24 09:44:31 +0200

    Update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import CookiesNoticeWrapper from './src/components/cookies-notice-wrapper/cookies-notice-wrapper';
 import './src/global/css/smartbanner.min.css';
-import './src/global/css/global.css';
 
 export const wrapPageElement = ({ element, props }) => {
   return (
