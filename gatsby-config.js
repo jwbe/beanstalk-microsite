05df3d311674ec66011545e08d036c9450987c56
JamesBarrett <james.barrett.works@gmail.com>
2020-08-25 16:15:57 +0200

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,21 +59,6 @@ module.exports = {
       options: {
         output: `/sitemap.xml`,
         exclude: [`/refer`, `/beanstalk-survey`, `/jisa-declaration`, `/isa-declaration`, `/key-features-documents`, `/key-investor-information-documents`, `/articles/about-junior-isa-app`, `/articles/how-family-saving-works-app`, `/articles/how-round-ups-work-app`, `/articles/locate-a-missing-ctf-app`, `/articles/saving-vs-investing-app`, `/articles/transfer-ctf-jisa-app`, `/articles/welcome-to-beanstalk-app`],
-        query: `
-        {
-          allMdx(filter: {frontmatter: {canonical: {eq: null}}, fields: {collection: {in: ["pages", "articles"]}}}) {
-            edges {
-              node {
-                fields {
-                  collection
-                }
-                frontmatter {
-                  slug
-                }
-              }
-            }
-          }
-        }`,
         serialize: ({ site, allSitePage, allMdx }) => {
           let pages = []
           const allEdges = allMdx.edges;
